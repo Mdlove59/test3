548ddf2b07e12076e3f0cbd2749b803858194ee1
Mdlove59 <>
2025-06-05 12:54:15 +0200

    Last Sync: 2025-06-05 10:54:15 (Mobile)

---
+++ device.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/xiaomi/agate
+
+ALLOW_MISSING_DEPENDENCIES := true
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.1-service  \
+    android.hardware.boot@1.2-service \
+    libmtk_bsg.recovery
+
+PRODUCT_PACKAGES+= \
+    bootctrl.mt6893
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
